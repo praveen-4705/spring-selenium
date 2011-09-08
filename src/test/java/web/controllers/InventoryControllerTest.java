fd90c95c2fe339dce9ebe93b5569d8a60f367d15
Li Ellis Gallardo <lellisga@gmail.com>
2011-09-08 09:19:38 -0500

    add test to inventory controller and inject products from servlet

--- src/test/java/web/controllers/InventoryControllerTest.java
+++ src/test/java/web/controllers/InventoryControllerTest.java
@@ -1,17 +1,23 @@
 package web.controllers;
 
+import java.util.Map;
+
 import junit.framework.TestCase;
 
 import org.springframework.web.servlet.ModelAndView;
+
+import service.SimpleProductManager;
 
 public class InventoryControllerTest extends TestCase {
 
 	 public void testHandleRequestView() throws Exception{
 	        InventoryController controller = new InventoryController();
+	        controller.setProductManager(new SimpleProductManager());	        
 	        ModelAndView modelAndView = controller.handleRequest(null, null);
 	        assertEquals("web/hello.jsp", modelAndView.getViewName());
 	        assertNotNull(modelAndView.getModel());
-	        String nowValue = (String) modelAndView.getModel().get("now");
+	        Map modelMap = (Map) modelAndView.getModel().get("model");
+	        String nowValue = (String) modelMap.get("now");
 	        assertNotNull(nowValue);
 	    }
     
