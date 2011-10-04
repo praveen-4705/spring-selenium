51eaedd14eebb4027fa514dcdaed42f424635ab9
Li Ellis Gallardo <lellisga@gmail.com>
2011-10-04 13:07:13 -0500

    selenium pilot test is working

--- src/test/java/behavior/indexPilotTest.java
+++ src/test/java/behavior/indexPilotTest.java
@@ -1,6 +1,10 @@
 package behavior;
 
+import org.openqa.selenium.server.SeleniumServer;
 import org.testng.AssertJUnit;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -8,21 +12,35 @@ import com.thoughtworks.selenium.SeleneseTestCase;
 
 public class indexPilotTest extends SeleneseTestCase{
 	
+	private SeleniumServer server;
+	
+	@BeforeClass
+	public void startServer() throws Exception{
+		server = new SeleniumServer();
+		server.start();
+	}
+	
+	@AfterClass
+	public void stopServer(){
+		server.stop();
+	}
+	
+	@AfterMethod
+	public void stopSeleniumInstance(){
+		selenium.stop();
+	}
+		
 	@BeforeMethod	
 	public void setUp() throws Exception{	
 		setUp("http://www.google.com/", "*firefox");
 	}
 	
 	@Test
-	public void testNew() throws Exception{
-		
+	public void testNew() throws Exception{		
 		selenium.open("/");
 		selenium.type("q", "selenium rc");
-		selenium.click("btnG");		
-		boolean present = true;
-		present = selenium.isTextPresent("google");		
-//		AssertJUnit.assertTrue(present);
-		selenium.stop();
+		selenium.click("btnG");
+		AssertJUnit.assertTrue(selenium.isTextPresent("Google"));
 	}
 
 }
