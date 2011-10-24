98b561ba05cce00ae4a3c1b3593379bd751118ed
Li Ellis Gallardo <lellisga@gmail.com>
2011-10-24 16:25:18 -0500

    add session to controller

--- src/main/java/web/controllers/forms/LoginFormController.java
+++ src/main/java/web/controllers/forms/LoginFormController.java
@@ -1,9 +1,16 @@
 package web.controllers.forms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.validation.BindException;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.SimpleFormController;
 import org.springframework.web.servlet.view.RedirectView;
@@ -14,10 +21,12 @@ public class LoginFormController extends SimpleFormController{
 	
     protected final Log logger = LogFactory.getLog(getClass());   
     
-    public ModelAndView onSubmit(Object command) throws ServletException {
-    	    UserLogin userLogin = (UserLogin) command;    	    
-    		logger.info(userLogin.getUserName() + " Is a valid User");
+    public ModelAndView onSubmit(HttpServletRequest request, HttpServletResponse response,Object command,BindException errors) throws ServletException {
+    	    HttpSession session = request.getSession();
+    		UserLogin userLogin = (UserLogin) command;    	    
+    		session.setAttribute("userInfo",userLogin );	    		
+    		logger.info(userLogin.getUserName() + " Is a valid User");    		
     		logger.info("Redirecting to " + getSuccessView());
     		return new ModelAndView(new RedirectView(getSuccessView(),true));
-    }
+    }    
 }
