ddedb33bff0c398994243c122d04eb8642a7f85f
Li Ellis Gallardo <lellisga@gmail.com>
2011-10-24 16:24:07 -0500

    add session and logOut function

--- src/main/java/web/controllers/HomeController.java
+++ src/main/java/web/controllers/HomeController.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openqa.jetty.jetty.servlet.AbstractSessionManager.Session;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
@@ -30,9 +32,12 @@ public class HomeController extends MultiActionController{
 	public ModelAndView home(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		
+		HttpSession session = request.getSession();
+		boolean hasPriv = session.getAttribute("userInfo") != null;
 		Map<String, Object> myModel = new HashMap<String, Object>();		
 		logger.info("Calling home method");		
 		myModel.put("products", productManager.getProducts());
+		myModel.put("hasPriv", hasPriv);
 		
         return new ModelAndView("web/home/home.jsp", "model", myModel);
 
@@ -44,5 +49,16 @@ public class HomeController extends MultiActionController{
 
 		return new ModelAndView("web/home/about.jsp");
 	}
+	
+	public ModelAndView logOut(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		logger.info("Login Out");		
+		HttpSession session = request.getSession();
+		session.removeAttribute("userInfo");
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("products", productManager.getProducts());
+		return new ModelAndView("web/home/home.jsp","model",myModel);
+	}
+
 
 }
